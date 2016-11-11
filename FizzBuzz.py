9bbbd29074fd9f8b6740a08471fb53a21af5c677
Alvin <alvinmutisya@gmail.com>
2016-11-11 12:41:44 +0300

    A function fizz_buzz that returns 'Fizz', 'Buzz', 'FizzBuzz', or the argument it receives, all depending on the argument of the function, a number that is divisible by, 3, 5, or both 3 and 5, respectively.

---
+++ FizzBuzz.py
@@ -0,0 +1,11 @@
+def fizz_buzz(number):
+	if number% 3 is 0 and number % 5 is 0:
+		return "FizzBuzz"
+	elif number% 3 is 0:
+		return "Fizz"
+	elif number % 5 is 0:
+		return "Buzz"
+	else:
+		return number
+
+print fizz_buzz(5)
